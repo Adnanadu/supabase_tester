305ecb53688ed125d49ccad9cc4af7e8d37bac96
AdnanAdu <adnanpe5@gmail.com>
2025-03-08 16:35:46 +0530

    added login page

--- lib/feature/auth/pages/login_page.dart
+++ lib/feature/auth/pages/login_page.dart
@@ -1,10 +1,58 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class LoginPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:supa_base_tester/feature/auth/auth_services.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //get auth services
+  final authServices = AuthServices();
+
+  //text controllers
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  //login button Function
+  void login() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    //sign in with email and password
+
+    try {
+      await authServices.signInWithEmailAndPassword(email, password);
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Failed to sign in!')));
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          TextField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+          ),
+          ElevatedButton(onPressed: login, child: const Text('Login')),
+        ],
+      ),
+    );
   }
 }
