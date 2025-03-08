6ff3b271fa9226ef69baa1f66fbc48e0a6ae74b2
AdnanAdu <adnanpe5@gmail.com>
2025-03-08 16:30:01 +0530

    added auth gate

---
+++ lib/feature/auth/auth_gate.dart
@@ -0,0 +1,42 @@
+/*
+AUTH GATE - This will continuosly listen for auth state changes.
+
+---------------------------------------------------------------
+
+unauthentication  ->  Login Page
+authenticated    ->  Profile Page
+
+*/
+
+import 'package:flutter/material.dart';
+import 'package:supa_base_tester/feature/auth/pages/login_page.dart';
+import 'package:supa_base_tester/feature/auth/pages/profile_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' show Supabase;
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Supabase.instance.client.auth.onAuthStateChange,
+      builder: (context, snapshot) {
+        //loading...
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // check if there is a valid session currently
+        final session = snapshot.hasData ? snapshot.data!.session : null;
+
+        if (session != null) {
+          return const ProfilePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
--- lib/feature/auth/pages/login_page.dart
+++ lib/feature/auth/pages/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
-  const SignInPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
